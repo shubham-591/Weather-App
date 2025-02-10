a933cde475299568f5e875de8e296dbde14f15ae
shubham-591 <130385835+shubham-591@users.noreply.github.com>
2025-02-10 11:55:58 +0530

    Final App with dynamic UI

--- backend/routes/weather.js
+++ backend/routes/weather.js
@@ -11,11 +11,17 @@ router.get("/:city", async (req, res) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPENWEATHER_API_KEY}&units=metric`;
 
     const response = await axios.get(url);
+    const weatherData = response.data;
     res.json({
-      city: response.data.name,
-      temp: response.data.main.temp,
-      humidity: response.data.main.humidity,
-      description: response.data.weather[0].description,
+      // city: response.data.name,
+      // temp: response.data.main.temp,
+      // humidity: response.data.main.humidity,
+      // description: response.data.weather[0].description,
+      city: weatherData.name,
+      temp: weatherData.main.temp,
+      humidity: weatherData.main.humidity,
+      windSpeed: weatherData.wind.speed,
+      icon: weatherData.weather[0].icon,
     });
   } catch (error) {
     res.status(500).json({ error: "Error fetching weather data" });
